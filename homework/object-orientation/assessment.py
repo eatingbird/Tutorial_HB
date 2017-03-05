73445c04d19fe6d0753638a4ac026c5e568c21d5
eatingbird <consulthere@gmail.com>
2017-03-05 10:15:22 -0800

    2nd week hw added

---
+++ homework/object-orientation/assessment.py
@@ -0,0 +1,46 @@
+"""
+Part 1: Discussion
+
+1. What are the three main design advantages that object orientation
+   can provide? Explain each concept.
+
+   Abstraction: Hiding details unneed
+   Encaptulation: Keeping everything together
+   Polymorphism: Interchangablility of components
+
+2. What is a class?
+
+   Code that encaptulates definitions of similar type of things.
+
+3. What is an instance attribute?
+
+   A characteristic that is defined for an individual occurence 
+   of a class 
+
+4. What is a method?
+
+   A function that defined on a class
+
+5. What is an instance in object orientation?
+
+   A manifestation of a class
+
+6. How is a class attribute different than an instance attribute?
+   Give an example of when you might use each.
+
+   Class attribute is a character that will inherit to the 
+   instances. Instance attribute is a character that defines
+   the occurence.
+   For example, in "animal" class, there may be a quality that
+   is "with blood". In "reptile" which is an instance of the
+   class "animal", "with blood" attribute will be inherited,
+   but the reptile also may have "cold blood" attribute which
+   is different from the "mannal" instance that will have
+   the attribute of "warm blood"
+"""
+
+
+# Parts 2 through 5:
+# Create your classes and class methods
+
+
